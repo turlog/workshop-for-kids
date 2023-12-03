7a284dbc9a950ff9ed62a3ca6fb8a68d988f2572
Łukasz Zosiak <lukasz.zosiak@gmail.com>
2023-12-03 22:07:10 +0100

    new bootstrap script (2023) for Windows 11 SurfaceBook

---
+++ bootstrap/bootstrap.ps1
@@ -0,0 +1,34 @@
+﻿# Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+
+Install-PackageProvider -Name NuGet -Force
+Install-Module PSReadLine -Force
+
+$apps = @(
+@{id = "7zip.7zip" },
+@{id = "Git.Git" },
+@{id = "GitHub.cli" },
+@{id = "Microsoft.VisualStudioCode" },
+@{id = "Microsoft.WindowsTerminal" },
+@{id = "Anaconda.Miniconda3" }
+);
+
+$extensions = @(
+@{id = "ms-python.python" },
+@{id = "ms-vscode.PowerShell" },
+@{id = "MS-CEINTL.vscode-language-pack-pl" }
+);
+
+Foreach ($app in $apps) {
+   Write-Host "Installing " $app.id
+#   $installed = winget list --accept-source-agreements --accept-package-agreements  --id $app.id
+#   if (![String]::Join("", $installed).Contains($app.id)) {
+       winget install --accept-source-agreements --accept-package-agreements --id $app.id
+#   }
+}
+
+#runas /trustlevel:0x20000 "conda update -n base -y conda"
+
+Foreach ($extension in $extensions) {
+   & $env:LOCALAPPDATA"\Programs\Microsoft VS Code\bin\code.cmd" --force --install-extension $extension.id
+}
+
